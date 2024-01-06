cdf9645c183ffe3b8290d46702616ff8fa0a1de2
Hiếu Trần Phúc <phuchieu20.01@gmail.com>
2024-01-06 21:20:55 +0700

    BaiTap06Jan

---
+++ helper/configs.js
@@ -0,0 +1,10 @@
+module.exports= {
+    SECRET_KEY:"heheheehe",
+    EXP: '20d',
+    cookieExp: 2,
+    MAIL_HOST:'sandbox.smtp.mailtrap.io',
+    MAIL_PORT:2525,
+    MAIL_USER: '208de4ed6862b7',
+    MAIL_PASS:'65bb1c1fdace02',
+
+}
